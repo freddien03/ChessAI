559e2abd309228430b0a336e51893b4d002ad569
freddien03 <60051731+freddien03@users.noreply.github.com>
2021-08-15 14:51:30 +0100

    Initial Commit

---
+++ ChessAI/ChessAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAIApp.swift
+//  ChessAI
+//
+//  Created by Freddie Nunn on 15/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChessAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
